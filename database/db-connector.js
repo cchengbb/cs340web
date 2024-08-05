ae9d50676dde7e7de01ba510d80b1cd449481682
Roy <huynhro@oregonstate.edu>
2024-08-05 16:11:36 -0700

    Styling overhaul

--- database/db-connector.js
+++ database/db-connector.js
@@ -1,16 +1,16 @@
 // ./database/db-connector.js
 
 // Get an instance of mysql we can use in the app
-var mysql = require('mysql')
+var mysql = require("mysql");
 
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
-    connectionLimit : 20,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_chenlinl',
-    password        : '1234',
-    database        : 'cs340_chenlinl'
-})
+	connectionLimit: 20,
+	host: "classmysql.engr.oregonstate.edu",
+	user: "cs340_huynhro",
+	password: "3839",
+	database: "cs340_huynhro",
+});
 
 // Export it for use in our applicaiton
 module.exports.pool = pool;
